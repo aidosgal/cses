7a724fbf20c40e6bed8389d73f93ce6a7ef8c6f6
aidosgal <agalimzhan928@gmail.com>
2025-03-20 12:21:06 +0500

    feat(leetcode): solve the product of array

--- leetcode/array.cpp
+++ leetcode/array.cpp
@@ -1,4 +1,5 @@
 #include "array.hpp"
+#include <string.h>
 
 bool Solution::containsDuplicate(std::vector<int>& nums) {
     std::unordered_set<int> seen;
@@ -29,4 +30,86 @@ bool Solution::isAnagram(std::string s, std::string t) {
         count[c]--;
     }
     return true;
+}
+
+std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int dif = target - nums[i];
+        if (map.find(dif) != map.end()) {
+            return {map[dif], i};
+        }
+
+        map.insert({nums[i], i});
+    }
+
+    return {};
+}
+
+std::vector<std::vector<std::string>> Solution::groupAnagrams(std::vector<std::string>& strs) {
+    std::unordered_map<std::string, std::vector<std::string>> map;
+
+    for (std::string s : strs) {
+        std::vector<int> l(26, 0);
+        for (char c : s) {
+            l[c - 'a']++;
+        }
+        std::string key = std::to_string(l[0]);
+        for (int i = 0; i < 26; i++) {
+            key += ',' + std::to_string(l[i]);
+        }
+        map[key].push_back(s);
+    }
+    
+    std::vector<std::vector<std::string>> ans;
+
+    for (const auto& pair : map) {
+        ans.push_back(pair.second);
+    }
+
+    return ans;
+}
+
+std::vector<int> Solution::topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> count;
+    std::vector<std::vector<int>> freq(nums.size() + 1);
+    for (int num : nums) {
+        count[num] = 1 + count[num];
+    }
+    for (const auto& pair : count) {
+        freq[pair.second].push_back(pair.first);
+    }
+
+    std::vector<int> res;
+    for (int i = freq.size() - 1; i > 0; --i) {
+        for (int n : freq[i]) {
+            res.push_back(n);
+            if (res.size() == k) {
+                return res;
+            }
+        }
+    }
+
+    return res;
+}
+
+std::vector<int> Solution::productExceptSelf(std::vector<int>& nums) {
+    std::vector<int> res(nums.size(), 1);
+
+    for(int i = 1; i < nums.size(); i++) {
+        res[i] = res[i-1] * nums[i-1];
+    }
+
+    int p = 1;
+    for (int i = nums.size() - 1; i >= 0; --i) {
+        res[i] *= p;
+        p *= nums[i];
+    }
+
+    return res;
+}
+
+bool Solution::isValidSudoku(std::vector<std::vector<char>>& board) {
+    return false;
 }
--- leetcode/array.hpp
+++ leetcode/array.hpp
@@ -8,6 +8,10 @@ class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums);
     bool isAnagram(std::string s, std::string t);
+    std::vector<int> twoSum(std::vector<int>& nums, int target);
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs);
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k);
+    std::vector<int> productExceptSelf(std::vector<int>& nums);
+    bool isValidSudoku(std::vector<std::vector<char>>& board);
 };
-
 #endif
