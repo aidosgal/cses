f87d194fe550de2a93540f247a20b30563ae2041
aidosgal <agalimzhan928@gmail.com>
2025-03-24 14:27:35 +0500

    feat(leetcode): solve the bs in rotated array

--- leetcode/go/main.go
+++ leetcode/go/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
-	"sort"
 	"unicode"
+n	"sort"
 )
 
 type stack []interface{}
@@ -262,4 +262,56 @@ func minEatingSpeed(piles []int, h int) int {
 		}
 	}
     return ans
+}
+
+func findMin(nums []int) int {
+	ans := nums[0]
+	l, r := 0, len(nums) - 1
+	for l <= r {
+		if nums[l] < nums[r] {
+			if nums[l] < ans {
+				ans = nums[l]
+				break
+			}
+			
+			mid := (r+l)/2
+			if ans > nums[mid] {
+				ans = nums[mid]
+			}
+			
+			if nums[mid] >= nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return ans
+}
+
+func searchRotatedArray(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := (l + r)/2
+		if target == nums[mid] {
+			return mid
+		}
+
+		if nums[l] <= nums[mid] {
+			if target > nums[mid] || target < nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		} else {
+			if target < nums[mid] || target > nums[r] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+}
+		}
+	}
+	return -1
 }
