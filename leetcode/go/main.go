1c3a00a0d886d40e5a7ba2a5cfc63bdd54d2577a
aidosgal <agalimzhan928@gmail.com>
2025-03-24 14:56:03 +0500

    feat(leetcode): solve the timestamp map

--- leetcode/go/main.go
+++ leetcode/go/main.go
@@ -310,8 +310,57 @@ func searchRotatedArray(nums []int, target int) int {
 			} else {
 				l = mid + 1
 			}
-}
 		}
 	}
 	return -1
 }
+
+type TimeMap struct {
+	m map[string][]pair
+}
+
+type pair struct {
+	time int
+	value string
+}
+
+func Constructor() TimeMap {
+	return TimeMap{
+		m: make(map[string][]pair),
+	}
+}
+
+
+func (this *TimeMap) Set(key string, value string, timestamp int)  {
+	this.m[key] = append(this.m[key], pair{value: value, time: timestamp})
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	if _, e := this.m[key]; !e {
+		return ""
+	}
+
+	pairs := this.m[key]
+
+	l, r := 0, len(pairs)-1
+
+	for l <= r {
+		mid := (l + r)/2
+		if pairs[mid].time <= timestamp {
+			if mid == len(pairs)-1 || pairs[mid+1].time > timestamp {
+				return pairs[mid].value
+			}
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ""
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
