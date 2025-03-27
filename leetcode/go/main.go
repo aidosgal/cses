f70c67824137095eec958bf9512f1ca9853c443e
aidosgal <agalimzhan928@gmail.com>
2025-03-27 17:19:19 +0500

    feat(leetcode): solve the linked list add two

--- leetcode/go/main.go
+++ leetcode/go/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 	"unicode"
-n	"sort"
+	"sort"
 )
 
 type stack []interface{}
@@ -364,3 +364,34 @@ func (this *TimeMap) Get(key string, timestamp int) string {
  * obj.Set(key,value,timestamp);
  * param_2 := obj.Get(key,timestamp);
  */
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    current := dummy
+    carry := 0
+
+    for l1 != nil || l2 != nil || carry != 0 {
+        val1, val2 := 0, 0
+        if l1 != nil {
+            val1 = l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            val2 = l2.Val
+            l2 = l2.Next
+        }
+
+        sum := val1 + val2 + carry
+        carry = sum / 10
+
+        current.Next = &ListNode{Val: sum % 10}
+        current = current.Next
+    }
+
+    return dummy.Next
+}
