05ce512723ddcf853b3598f102b965e3c8ad9945
aidosgal <agalimzhan928@gmail.com>
2025-04-14 12:02:00 +0500

    feat(leetcode): find the count of good triplets

--- leetcode/go/main.go
+++ leetcode/go/main.go
@@ -670,3 +670,29 @@ func isSymmetric(num int) bool {
 
 	return true
 }
+
+func countGoodTriplets(arr []int, a int, b int, c int) int {
+	result := 0
+	for i := 0; i < len(arr) - 2; i++ {
+        for j := i + 1; j < len(arr) - 1; j++ {
+            if mod(arr[i] - arr[j]) > a {
+                continue
+            }
+
+            for k := j + 1; k < len(arr); k++ {
+                if mod(arr[j] - arr[k]) <= b && mod(arr[i] - arr[k]) <= c {
+                    result++
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+func mod(n int) int {
+    if n < 0 {
+        return -n
+    }
+    return n
+}
